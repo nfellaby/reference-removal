4c0c798e6bae0166dfaaf870fddf99bfb292c6ad
nicholas ellaby <nfellaby@gmail.com>
2026-09-08 15:31:21 +0100

    feat: added report generation

--- bin/generate_validation_report.py
+++ bin/generate_validation_report.py
@@ -9,7 +9,7 @@ import pyfastx
 
 
 def trim_read_id(read_id):
-    if read_id.endswith("/1") or read_id.endswith("/2"):
+    if read_id.endswith(("/1", "/2")):
         read_id = read_id[:-2]
     return read_id
 
